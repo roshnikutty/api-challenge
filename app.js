59f22826c3968a8e1656977cc0fe905e1d82dd1e
Roshni Kutty <roshni.ajkutty@gmail.com>
2017-11-01 22:08:12 -0400

    implemented all 3 APIs and sorted by score

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 let TOP_NEWS_URL = "https://hacker-news.firebaseio.com/v0/topstories.json";
 let STORY_URL = "https://hacker-news.firebaseio.com/v0/item/";
 let AUTHOR_URL = "https://hacker-news.firebaseio.com/v0/user/";
-var top_ten_newsIds = [];
-var storiesArray = [];
+let sortedArray = [];
+
 //Storing state of the program
 let state = {
-    randomTopNewsIndex: []
+    randomTopNewsIndex: [],
+    sortedStories: []
 };
 
 //Check for unique indices in the array
@@ -32,7 +33,9 @@ let randomTopNewsIDGenerator = () => {
 randomTopNewsIDGenerator();
 
 //API call to TOP_NEWS_URL, function retrns 10 random top news ids
-function getNewsDetails() {
+let getNewsDetails = () => {
+    let top_ten_newsIds = [];
+    let storiesArray = [];
     $.getJSON(TOP_NEWS_URL).done((data) => {                                                        //API for top news
         for (let i = 0; i < state.randomTopNewsIndex.length; i++) {
             top_ten_newsIds.push(data[i]);
@@ -51,7 +54,7 @@ function getNewsDetails() {
                             for (let i = 0; i < storiesArray.length; i++) {
                                 //'by' key of the Story API should be the same as 
                                 //the author's 'id' key from the Author API
-                                if (storiesArray[i].author === author.id) {                         
+                                if (storiesArray[i].author === author.id) {
                                     storiesArray[i].karma = author.karma
                                 }
                             }
@@ -59,7 +62,59 @@ function getNewsDetails() {
                 });
         }
     })
-    state.storiesArray = storiesArray;
+    setTimeout(function () {
+        state.storiesArray = storiesArray;
+    }, 1000);
+    // state.storiesArray = storiesArray;
 }
-getNewsDetails();                                                                      
-console.log(state.storiesArray);                                                    //logs in the console all data from the three API and 10 random indices of top news
+getNewsDetails();
+
+setTimeout(function () {
+    console.log(sort());
+}, 1000);
+
+
+
+//Sort output by story score
+let sort = () => {
+    sortedArray = state.storiesArray;
+    sortedArray.sort(function (a, b) {
+        return b.score - a.score;
+    });
+    setTimeout(function () {
+        state.sortedArray = sortedArray;
+    }, 1000);
+    return sortedArray;
+}
+                                
+
+//-------------------  Page display  -------------------------
+/*
+let displayData = () => {
+    randomTopNewsIDGenerator();
+    getNewsDetails();
+    sort();
+    var resultElement = "";
+    if (state.sortedArray.length) {
+        state.sortedArray.forEach(function (item) {
+            resultElement = resultElement +
+                `<p>Story Title: ${item.title} </p>
+                <p>Story URL: ${item.story_url}</p>
+                <p>Story timestamp to Date: ${item.story_time}</p>       
+                <p>Story score: ${item.score}</p>
+                <p>Author id: ${item.author}</p>
+                <p>Author karma score: ${item.karma}</p>`;
+        });
+    }
+    else {
+        return "No results were found.";
+    }
+    $(".js-results").html(resultElement);
+}
+
+
+$(function(){
+  displayData();
+});
+
+*/
